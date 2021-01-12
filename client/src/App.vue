26d1437ecc45625aa8a5222dd1bf44989d014d3d
kingkirpal2000 <skirpal2000@gmail.com>
2021-01-12 01:58:18 -0800

    Changed layout of CSS to css grid and it looks fine. Flex box does not work for my purpose

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,21 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <router-view />
   </div>
-  <router-view/>
 </template>
 
 <style>
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Anonymous, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
 #nav {
